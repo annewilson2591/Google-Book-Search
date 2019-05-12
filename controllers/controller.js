74f2cb448977e0cfe129f9047d95bf08d3bf2ab9
Anne Wilson <annewilson2591@gmail.com>
2019-05-12 14:12:13 -0600

    skeleton file structure

---
+++ controllers/controller.js
@@ -0,0 +1,25 @@
+const db = require("../models");
+
+module.exports = {
+    findBooks: function(req, res) {
+        db.Book
+            .find(req.query)
+            .then(bookData => res.json(bookData))
+            .catch(error => console.log(error));
+    },
+    
+    addBook: function(req, res) {
+        db.Book
+            .create(req.body)
+            .then(result => res.json(result))
+            .catch(error => console.log(error));
+    },
+
+    removeBook: function(req, res) {
+        db.Book
+            findById({ _id: req.params.id })
+            .then(bookData => bookData.remove())
+            .then(result => res.json(result))
+            .catch(error => console.log(error));
+    }
+}
