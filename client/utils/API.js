8ef6b573710ced0e0e32621d67c7158597ada890
Anne Wilson <annewilson2591@gmail.com>
2019-05-12 16:25:53 -0600

    added API under utils

---
+++ client/utils/API.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotennv");
+const axios = require("axios");
+
+export default {
+    searchByKeyword: function(keyword) {
+        // return axios.get(API KEY HERE)
+    },
+
+    searchByKeywordAndAuthor: function(keyword, author) {
+            // return axios.get(API KEY HERE)
+    },
+
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+
+    addBook: function(data) {
+        return axios.post("/api/books", data);
+    },
+
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
+}
