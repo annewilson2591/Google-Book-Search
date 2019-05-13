1ca38f84f10fca92a21770f1cffdcf411af9f9bc
Anne Wilson <annewilson2591@gmail.com>
2019-05-13 13:10:33 -0600

    added footer

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -3,14 +3,6 @@ import './style.css';
 
 export default function Footer() {
     return (
-        <footer>
-            <a
-            href="https://www.google.com/intl/en/googlebooks/about/index.html"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="google-books">
-            About Google Books    
-            </a>
-        </footer>
+        <footer />
     )
 }
---
+++ client/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css';
+
+export default function Header() {
+    return (
+        <div className="head-style">
+            <header className="head-content">
+                <h1>(React) Google Book Search</h1>
+                <h3>Search for Books and Save Favorites</h3>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
